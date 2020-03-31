877330e2cac39f3e109cef9b29617ec311408016
MyTh NooTy <mythpe@gmail.com>
2020-03-31 23:42:37 +0300

    Copyright

--- src/Providers/JawalBSmsProvider.php
+++ src/Providers/JawalBSmsProvider.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright MyTh
+ * Website: https://4MyTh.com
+ * Email: mythpe@gmail.com
+ * Copyright © 2006-2020 MyTh All rights reserved.
+ */
 
 namespace MyTh\Support\JawalBSms\Providers;
 
